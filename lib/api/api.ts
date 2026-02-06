17a4e53079472524dff333e919ecff8140176a90
Yaroslav <yazik.tariz@gmail.com>
2026-02-06 23:54:24 +0200

    fix problem

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
 
-const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+// const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+
+// export const api = axios.create({
+//   baseURL,
+//   withCredentials: true,
+// });
+const envBase =
+  process.env.NEXT_PUBLIC_API_URL
+    ? `${process.env.NEXT_PUBLIC_API_URL}/api`
+    : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api`
+      : "/api";
 
 export const api = axios.create({
-  baseURL,
+  baseURL: envBase,
   withCredentials: true,
 });
